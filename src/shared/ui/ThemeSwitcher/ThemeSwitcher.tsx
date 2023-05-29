50807fb318cc705c23773784a9aa5a3b2fd102ba
y.yakubovich <y.yakubovich@softteco.com>
2023-05-29 14:00:59 +0300

    Added Stylelint

--- src/shared/ui/ThemeSwitcher/ThemeSwitcher.tsx
+++ src/shared/ui/ThemeSwitcher/ThemeSwitcher.tsx
@@ -1,5 +1,4 @@
 import { cn } from 'shared/lib/classNames/classNames'
-import cls from './ThemeSwitcher.module.scss'
 import { Theme, useTheme } from 'app/providers/ThemeProvider'
 import LightIcon from 'shared/assets/icons/theme-light.svg'
 import DarkIcon from 'shared/assets/icons/theme-dark.svg'
@@ -15,7 +14,7 @@ export const ThemeSwitcher = ({ className }: ThemeSwitcherProps) => {
     return (
         <Button
             theme={ThemeButton.PLAIN}
-            className={cn(cls.ThemeSwitcher, {}, [className])}
+            className={cn('', {}, [className])}
             onClick={toggleTheme}
         >
             {theme === Theme.DARK
