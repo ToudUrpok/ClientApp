a1cbd068b9f19cef4a575c45af6baf066511a3b8
y.yakubovich <y.yakubovich@softteco.com>
2023-06-08 11:18:58 +0300

    Added Loader component and used while page loading

---
+++ src/shared/ui/Loader/Loader.tsx
@@ -0,0 +1,16 @@
+import { cn } from 'shared/lib/classNames/classNames'
+import './Loader.scss'
+
+interface LoaderProps {
+    className?: string
+}
+
+export const Loader = ({ className }: LoaderProps) => {
+    return (
+        <div className={cn('lds-facebook', {}, [className])}>
+            <div/>
+            <div/>
+            <div/>
+        </div>
+    )
+}
---
+++ src/widgets/PageLoader/ui/PageLoader.tsx
@@ -0,0 +1,15 @@
+import { cn } from 'shared/lib/classNames/classNames'
+import cls from './PageLoader.module.scss'
+import { Loader } from 'shared/ui/Loader/Loader'
+
+interface PageLoaderProps {
+    className?: string
+}
+
+export const PageLoader = ({ className }: PageLoaderProps) => {
+    return (
+        <div className={cn(cls.PageLoader, {}, [className])}>
+            <Loader />
+        </div>
+    )
+}
