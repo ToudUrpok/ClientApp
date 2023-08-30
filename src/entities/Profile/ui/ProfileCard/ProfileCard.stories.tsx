efd6a8cf778e2d5f370a546e2079b4ef917d1fd2
y.yakubovich <y.yakubovich@softteco.com>
2023-08-30 14:46:17 +0300

    Fix

---
+++ src/entities/Profile/ui/ProfileCard/ProfileCard.stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator'
+import { Theme } from 'app/providers/ThemeProvider'
+import { ProfileCard } from './ProfileCard'
+
+const meta: Meta<typeof ProfileCard> = {
+    component: ProfileCard
+}
+
+export default meta
+type Story = StoryObj<typeof ProfileCard>
+
+export const Light: Story = {
+    args: {}
+}
+
+export const Dark: Story = {
+    args: {},
+    decorators: [
+        ThemeDecorator(Theme.DARK)
+    ]
+}
