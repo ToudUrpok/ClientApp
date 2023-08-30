847a027020475a90f8ab3f4b3260e232d9b5c2ec
y.yakubovich <y.yakubovich@softteco.com>
2023-08-30 13:45:24 +0300

    Fixed issue

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -4,34 +4,33 @@ import { Configuration, RuleSetRule, DefinePlugin } from 'webpack'
 import { buildCssLoader } from '../build/loaders/buildCssLoader'
 import { buildSvgLoader } from '../build/loaders/buildSvgLoader'
 
-export default ({ config }: { config: Configuration }) => {
+export default ({ config }: { config: Configuration }): Configuration => {
     const paths: BuildPaths = {
         build: '',
         html: '',
         entry: '',
         src: path.resolve(__dirname, '..', '..', 'src')
     }
+
     config.resolve?.modules?.push(paths.src)
     config.resolve?.extensions?.push('.ts', '.tsx')
 
-    if (config.module) {
-        config.module.rules = config.module.rules?.map((rule) => {
-            const typedRule = rule as RuleSetRule
-            // eslint-disable-next-line @typescript-eslint/prefer-includes
-            if (typedRule && (typedRule?.test as string).includes('svg')) {
-                return { ...typedRule, exclude: /\.svg$/i }
-            }
+    // @ts-expect-error
+    config.module.rules = config.module?.rules?.map((rule: RuleSetRule) => {
+        // eslint-disable-next-line @typescript-eslint/prefer-includes
+        if (/svg/.test(rule.test as string)) {
+            // Silence the Storybook loaders for SVG files
+            return { ...rule, exclude: /\.svg$/i }
+        }
 
-            return rule
-        })
-
-        config.module.rules?.push(buildSvgLoader())
-        config.module.rules?.push(buildCssLoader(true))
-    }
+        return rule
+    })
+    config.module?.rules?.push(buildSvgLoader())
+    config.module?.rules?.push(buildCssLoader(true))
     config.plugins?.push(
         new DefinePlugin({
             __IS_DEV__: true,
-            __API_BASE_URL__: ''
+            __API_BASE_URL__: JSON.stringify('')
         }))
 
     return config
